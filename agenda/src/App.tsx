bd2220963552e5c3faef1e879426c5ffebcaec4e
AndreBarroso34 <a14381@oficina.pt>
2024-10-06 21:26:17 +0100

    Primeiro commit

---
+++ agenda/src/App.tsx
@@ -0,0 +1,16 @@
+// Todos os imports tsx
+import CalendarApp from "./components/CalendarApp"
+
+// Todos os imports CSS
+import './components/CalendarApp.css'
+
+const App = () => {
+  return (
+    <div className="container">
+      < CalendarApp/>
+    </div>
+  )
+}
+
+export default App
+
